3f77c713017f534c8b9ab8329273ea69fd0001b3
maiamn <maia.mangin25@gmail.com>
2021-10-14 12:07:06 +0200

    add driver ADC

---
+++ FichiersCode/Drivers/Include/Driver_ADC.h
@@ -0,0 +1,29 @@
+#ifndef MYADC_H
+#define MYADC_H
+#include "stm32f10x.h"
+#include "core_cm3.h" 
+
+/*
+<!> INUTILE POUR LE MOMENT <!>
+
+// Structure enum 
+typedef enum {CONT, SINGLE} T_CONFIG ;
+
+// STRUCTURE 
+typedef struct 
+{
+	ADC_TypeDef * ADC ; 
+	T_CONFIG config ;
+} MyADC_Struct_TypeDef ; 
+*/ 
+
+// Fonction d'initialisation 
+void MyADC_Init(ADC_TypeDef * ADC) ;
+
+// Fonction de configuration -> Fonctionnement en mode continuous uniquement
+void MyADC_Config(ADC_TypeDef * ADC, unsigned char sampleTime, char channel) ; 
+
+// Fonction start
+void MyADC_Start(ADC_TypeDef * ADC) ; 
+
+#endif 
---
+++ FichiersCode/Drivers/Source/Driver_ADC.c
@@ -0,0 +1,24 @@
+#include "stm32f10x.h"
+#include "Driver_ADC.h"
+
+// Fonction d'initialisation 
+void MyADC_Init(ADC_TypeDef * ADC) {
+	
+	
+}
+
+
+
+// Fonction de configuration -> Fonctionnement en mode continuous uniquement
+void MyADC_Config(ADC_TypeDef * ADC, unsigned char sampleTime, char channel) {
+
+
+}	
+
+
+
+// Fonction start
+void MyADC_Start(ADC_TypeDef * ADC) {
+	
+	
+}
