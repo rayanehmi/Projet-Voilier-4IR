bb461e935772dc962852e13c95f224f34a035f44
Luis97Lopez <lpzr97@gmail.com>
2021-10-18 16:18:06 +0200

    MyUSART

--- FichiersCode/Drivers/Source/MyUART.c
+++ FichiersCode/Drivers/Source/MyUART.c
@@ -1,5 +1,9 @@
 #include "../Include/MyUART.h"
 
+// Pointeurs de l'UART
+void (* pFuncUART1) (void);
+void (* pFuncUART3) (void);
+
 void MyUART_Init ( MyUART_Struct_TypeDef * UARTStructPtr ) 
 {
 	UARTStructPtr -> UART -> CR1 |= USART_CR1_UE ; //enable USART
@@ -19,7 +23,33 @@ void MyUART_Init ( MyUART_Struct_TypeDef * UARTStructPtr )
 
 void MyUART_ActiveIT(USART_TypeDef * UART , char Prio , void (*IT_function)(void) ) {
 	if (UART == USART1){
-		//NVIC_EnableIRQ();
-}
+		  NVIC_EnableIRQ(USART1_IRQn);
+      NVIC_SetPriority(USART1_IRQn,Prio);
+			pFuncUART1 = IT_function;
+	}else if(UART == USART3){
+			NVIC_EnableIRQ(USART3_IRQn);
+      NVIC_SetPriority(USART3_IRQn,Prio);
+		  pFuncUART3 = IT_function;
+	}
 }
 
+
+// Functions Handlers
+void USART1_IRQHandler ( void ){
+	//TIM1->SR &= ~(TIM_SR_UIF);
+	if (pFuncUART1 != 0) (*pFuncUART1) (); /* appel indirect de la fonction */
+}
+
+void USART3_IRQHandler ( void ){
+	//TIM2->SR &= ~(TIM_SR_UIF);
+	if (pFuncUART3 != 0) (*pFuncUART3) (); /* appel indirect de la fonction */
+}
+
+
+void MyUART_PutStr ( USART_TypeDef * UART, char* String){
+	// While in all the String
+	while(*String != '\0'){
+		UART->DR = (int)*String; 
+		String++;
+	}
+}
--- FichiersCode/Sources/main.c
+++ FichiersCode/Sources/main.c
@@ -1,5 +1,14 @@
 #include "stm32f10x.h"
+#include "MyUART.h"
 
 int main(void){
+	MyUART_Struct_TypeDef uart;
+	
+	RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
+	
+	uart.UART = USART1;
+	uart.UART_BaudRate = 9600;
+	
+	MyUART_Init(&uart);
 	while(1);
 }
