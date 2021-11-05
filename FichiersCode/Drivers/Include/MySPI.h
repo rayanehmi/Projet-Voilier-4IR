0a95962a41e92e70b4077142e10a2cde240a2017
maiamn <maia.mangin25@gmail.com>
2021-11-05 15:16:53 +0100

    Update MySPI.H -> Début des fonctions Write et Read

--- FichiersCode/Drivers/Include/MySPI.h
+++ FichiersCode/Drivers/Include/MySPI.h
@@ -3,6 +3,13 @@
 
 #include "stm32f10x.h"
 
+// Fonction d'initialisation
 void MySPI_Init(SPI_TypeDef * SPI) ; 
-	
+
+//Fonction d'écriture 
+void MySPI_Write(SPI_TypeDef * SPI, uint8_t setup) ; 
+
+// Fonction de lecture 
+uint16_t MySPI_Read() ; 
+
 #endif
