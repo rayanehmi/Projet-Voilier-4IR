f2674d5e45f3a7fa848397a8b84fc2026d93ec2a
rayanehmi <77632181+rayanehmi@users.noreply.github.com>
2021-11-05 14:15:28 +0100

    test plateau

--- FichiersCode/Sources/main.c
+++ FichiersCode/Sources/main.c
@@ -1,12 +1,15 @@
 #include "stm32f10x.h"
 #include "MyUART.h"
 #include "MyGPIO.h"
+#include "MyTimer.h"
+
 MyGPIO_Struct_TypeDef gpio;
 MyUART_Struct_TypeDef uart;
-char test;
+char controllerData;
+int vitesseRotationPlateau =0;
 
 void Callback(){
-	test = MyUART_GetChar(uart.UART);
+	controllerData = MyUART_GetChar(uart.UART);
 }
 
 int main(void){
@@ -15,7 +18,26 @@ int main(void){
 	
 	MyUART_Init(&uart);
 	MyUART_ActiveIT(uart.UART, 1, Callback);
+	//MyUART_PutStr(uart.UART, "ssstringgg");
 	
-	MyUART_PutStr(uart.UART, "ssstringgg");
-	while(1);
+	/* Partie rotation plateau*/
+	
+	MyTimer_PWM(TIM4,3);
+	
+	while(1){
+		
+		/* Starts listening on USART1 and sends rotation commands */
+		/* For the moment, PWM uses TIM4-chan3(PB8) and rotationDirection uses PB5 */
+
+		//Traitement
+		//if ( (int)controllerData != vitesseRotationPlateau) { //If the command changed
+			vitesseRotationPlateau = (int)controllerData;
+			//Commande
+			if (vitesseRotationPlateau<0) MyGPIO_Set(GPIOB,5);
+			else MyGPIO_Reset(GPIOB,5);
+			setCycle_PWM(TIM4,3,vitesseRotationPlateau);
+		//}
+		
+	}
+	
 }
