bea79f000f906e630025844b8b15e5195322e6ed
rayanehmi <77632181+rayanehmi@users.noreply.github.com>
2021-11-08 16:52:09 +0100

    Plateau qui marche !!!

--- FichiersCode/Sources/main.c
+++ FichiersCode/Sources/main.c
@@ -20,20 +20,18 @@ void Callback(){
 	/* For the moment, PWM uses TIM4-chan3(PB8) and rotationDirection uses PB5 */
 
 		//Traitement
-		//if (int)controllerData != vitesseRotationPlateau) //If the command changed
-			vitesseRotationPlateau = (signed char)controllerData;
+			vitesseRotationPlateau = (int) ((signed char)controllerData);
 			//Commande
-			if ( vitesseRotationPlateau >= 0)  {
+			if ( vitesseRotationPlateau <= 0)  {
 				MyGPIO_Set(GPIOB,5);
-				setCycle_PWM(TIM4,3,vitesseRotationPlateau);
-				MyTimer_Base_Start(TIM4);
+				setCycle_PWM(TIM4,3,(-1)*vitesseRotationPlateau);
 			}
 			else {
 				MyGPIO_Reset(GPIOB,5);
-				setCycle_PWM(TIM4,3,(-1)*vitesseRotationPlateau);
-				MyTimer_Base_Start(TIM4);
+				setCycle_PWM(TIM4,3,vitesseRotationPlateau);
 			}
 }
+
 
 int main(void){
 	
@@ -53,21 +51,25 @@ int main(void){
 	
 	//TIM4 channel 3
 	tim4_chan3.TimId=TIM4;
+		tim4_chan3.ARR=100-1;
+			tim4_chan3.PSC=1-1;
 	
 	//Init
 	MyUART_Init(&uart);
-	MyUART_ActiveIT(uart.UART, 1, Callback); //MyUART_PutStr(uart.UART, "ssstringgg");
+	MyUART_ActiveIT(uart.UART, 1, Callback); 
+	//MyUART_PutStr(uart.UART, "ssstringgg");
 	MyGPIO_Init(&gpioPB8);
 	MyGPIO_Init(&gpioPB5);
 	MyTimer_Base_Init(&tim4_chan3);
+	MyTimer_PWM(TIM4,3);
+
+	MyTimer_Base_Start(TIM4);
 	
 	/* Partie rotation plateau*/
 	
-	MyTimer_PWM(TIM4,3);
 
 	while(1){
-		int a = 0;
-		a = a + 1;
+			
 	}
 	
 }
