db214ea89207d2168314cc765654c96f25c0cf7b
maiamn <maia.mangin25@gmail.com>
2021-11-08 18:18:09 +0100

    SPI - Config + RW

--- FichiersCode/Drivers/Include/MySPI.h
+++ FichiersCode/Drivers/Include/MySPI.h
@@ -8,18 +8,10 @@
 void MySPI_Config(SPI_TypeDef * SPI) ; 
 
 //Fonction d'écriture 
-void MySPI_Write(SPI_TypeDef * SPI, uint8_t setup) ; 
-
-// Fonction de lecture 
-uint16_t MySPI_Read(SPI_TypeDef * SPI) ; 
+char MySPI_ReadWrite(SPI_TypeDef * SPI, char val) ; 
 
 
 
-// Structures des pin utilisées 
-MyGPIO_Struct_TypeDef GPIO_PA4_NSS = {GPIOA, 4, AltOut_Ppull} ;
-MyGPIO_Struct_TypeDef GPIO_PA5_SCK = {GPIOA, 5, AltOut_Ppull} ;
-MyGPIO_Struct_TypeDef GPIO_PA6_MISO = {GPIOA, 6, In_PullDown} ;
-MyGPIO_Struct_TypeDef GPIO_PA7_MOSI = {GPIOA, 7, AltOut_Ppull} ;
 
 
 // Macro de gestion du Chip Select 
--- FichiersCode/Drivers/Source/MySPI.c
+++ FichiersCode/Drivers/Source/MySPI.c
@@ -2,6 +2,12 @@
 #include "MySPI.h"
 #include "MyGPIO.h"
 
+// Structures des pin utilisées 
+MyGPIO_Struct_TypeDef GPIO_PA4_NSS = {GPIOA, 4, AltOut_Ppull} ;
+MyGPIO_Struct_TypeDef GPIO_PA5_SCK = {GPIOA, 5, AltOut_Ppull} ;
+MyGPIO_Struct_TypeDef GPIO_PA6_MISO = {GPIOA, 6, In_PullDown} ;
+MyGPIO_Struct_TypeDef GPIO_PA7_MOSI = {GPIOA, 7, AltOut_Ppull} ;
+
 
 void MySPI_Config(SPI_TypeDef * SPI) {
 	
@@ -11,7 +17,9 @@ void MySPI_Config(SPI_TypeDef * SPI) {
 	
 	// SPI_CR1 -> 3 4 5 bits pour définir serial clock baud rate 
 	// fSCLK/16 pour avoir inferieur a 5Mhz limite du ADXL345
-	SPI->CR1 |= 0x011 << 3 ; 
+	SPI->CR1 |= 0x1 << 3 ; 
+	SPI->CR1 |= 0x1 << 4 ; 
+	SPI->CR1 |= 0x0 << 5 ; 
 	
 	// Choisir bits CPOL et CPHA pour définir le mode de transfert de données 
 	// Fonctionnement de l'ADXL en CPOL=1 et CPHA=1
@@ -39,6 +47,24 @@ void MySPI_Config(SPI_TypeDef * SPI) {
 	MyGPIO_Init(& GPIO_PA6_MISO) ; 
 	MyGPIO_Init(& GPIO_PA7_MOSI) ; 
 	
-	////////// Configuring ADXL345 in slave mode //////////
 	
+}
+
+
+char MySPI_ReadWrite(SPI_TypeDef * SPI, char data) {
+	int a ; // Return value
+	
+	// Write //
+	while ((SPI->SR&SPI_SR_TXE)== 0) {
+		// Tant que que le registre TXE est plein alors on attend avant 
+	}
+	SPI->DR = data ; 
+	
+	// Read //
+	while ((SPI->SR&SPI_SR_RXNE)== 0) {
+		// Tant qu'il n'y a rien à lire, on attend
+	}
+	a=SPI_DR_DR ; 
+	
+	return a ; 
 }
