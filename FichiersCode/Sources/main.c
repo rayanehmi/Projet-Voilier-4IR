e39c8f5760cc3b60e294df46ff502f6b17969a03
Rayane HACHEMI <rhmi@outlook.fr>
2021-11-09 20:26:41 +0100

    Callback2
    Experimental : empêche les appels de courants qui Reset la carte Nucléo

--- FichiersCode/Sources/main.c
+++ FichiersCode/Sources/main.c
@@ -1,9 +1,8 @@
-//à faire : meilleure conversion hexa->dec , tourner dans l'autre sens
-
 #include "stm32f10x.h"
 #include "MyUART.h"
 #include "MyGPIO.h"
 #include "MyTimer.h"
+#define ACCELMAX 10
 
 MyGPIO_Struct_TypeDef gpioPB8;
 MyGPIO_Struct_TypeDef gpioPB5;
@@ -15,12 +14,11 @@ int vitesseRotationPlateau =0;
 void Callback(){
 	controllerData = MyUART_GetChar(uart.UART);
 	
-	
 	/* Starts listening on USART1 and sends rotation commands */
 	/* For the moment, PWM uses TIM4-chan3(PB8) and rotationDirection uses PB5 */
 
 		//Traitement
-			vitesseRotationPlateau = (int) ((signed char)controllerData);
+	vitesseRotationPlateau = (int) ((signed char)controllerData);
 			//Commande
 			if ( vitesseRotationPlateau <= 0)  {
 				MyGPIO_Set(GPIOB,5);
@@ -32,6 +30,25 @@ void Callback(){
 			}
 }
 
+int diff;
+void Callback2(){ //EXPERIMENTAL : Empeche des gros appels de courant
+	controllerData = MyUART_GetChar(uart.UART);
+	
+	controllerData = (int) ((signed char) controllerData);
+	diff = controllerData - vitesseRotationPlateau;
+	if (diff > ACCELMAX) vitesseRotationPlateau=ACCELMAX; //si il veut accelerer trop vite //see #define
+	else if (diff < -ACCELMAX) vitesseRotationPlateau=-ACCELMAX;
+	else vitesseRotationPlateau = diff;
+			//Commande
+			if ( vitesseRotationPlateau <= 0)  {
+				MyGPIO_Set(GPIOB,5);
+				setCycle_PWM(TIM4,3,vitesseRotationPlateau);
+			}
+			else {
+				MyGPIO_Reset(GPIOB,5);
+				setCycle_PWM(TIM4,3,vitesseRotationPlateau);
+			}
+}
 
 int main(void){
 	
@@ -56,7 +73,7 @@ int main(void){
 	
 	//Init
 	MyUART_Init(&uart);
-	MyUART_ActiveIT(uart.UART, 1, Callback); 
+	MyUART_ActiveIT(uart.UART, 1, Callback2); 
 	//MyUART_PutStr(uart.UART, "ssstringgg");
 	MyGPIO_Init(&gpioPB8);
 	MyGPIO_Init(&gpioPB5);
@@ -64,9 +81,6 @@ int main(void){
 	MyTimer_PWM(TIM4,3);
 
 	MyTimer_Base_Start(TIM4);
-	
-	/* Partie rotation plateau*/
-	
 
 	while(1){
 			
