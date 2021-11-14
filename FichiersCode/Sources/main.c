1016f5d1c52f7265d88195de7e039c854153ccf6
Luis97Lopez <lpzr97@gmail.com>
2021-11-14 17:47:45 +0100

    Add BordageAutomatique fonction.

--- FichiersCode/Sources/main.c
+++ FichiersCode/Sources/main.c
@@ -1,51 +1,9 @@
 #include "stm32f10x.h"
-#include "MyUART.h"
-#include "MyGPIO.h"
-#include "MyTimer.h"
-
-MyTimer_Struct_TypeDef timer;
-MyGPIO_Struct_TypeDef gpioA, gpioB;
+#include "MyBordageAutomatique.h"
 
 int main(void){
-	timer.TimId = TIM2;
-	timer.ARR = 1435;
-	timer.PSC = 1;
-	MyTimer_Base_Init(&timer);
-	
-	// CC2S ET CC3S
-	timer.TimId->CCMR1 |= TIM_CCMR1_CC1S;
-	timer.TimId->CCMR2 |= TIM_CCMR1_CC2S;
-	
-	// IC2F ET IC3F
-	timer.TimId->CCMR1 &=~ TIM_CCMR1_IC1F;
-	timer.TimId->CCMR2 &=~ TIM_CCMR1_IC2F;
-	
-	// CC2P ET CC3P
-	timer.TimId->CCER &=~ TIM_CCER_CC1P;
-	timer.TimId->CCER &=~ TIM_CCER_CC2P;
-	
-	// CC2NP ET CC3NP
-	timer.TimId->CCER &=~ TIM_CCER_CC1NP;
-	timer.TimId->CCER &=~ TIM_CCER_CC2NP;
-	
-	// SMS
-	timer.TimId->SMCR &=~ (TIM_SMCR_SMS);
-	timer.TimId->SMCR |= TIM_SMCR_SMS_0;
-	timer.TimId->SMCR |= TIM_SMCR_SMS_1;
-	
-	
-	gpioA.GPIO = GPIOA;
-	gpioA.GPIO_Pin = 0;
-	gpioA.GPIO_Conf = In_PullDown;
-	
-	gpioB.GPIO = GPIOA;
-	gpioB.GPIO_Pin = 1;
-	gpioB.GPIO_Conf = In_PullDown;
-	
-	MyGPIO_Init(&gpioA);
-	MyGPIO_Init(&gpioB);
-	
-	MyTimer_Base_Start(timer.TimId);
-	
+	MyBordageAutomatique_setup();
+	MyBordageAutomatique_start();
+
 	while(1);
 }
--- FichiersKeil/Objects/mygpio.d
+++ FichiersKeil/Objects/mygpio.d
@@ -1,7 +1,7 @@
 .\objects\mygpio.o: ..\FichiersCode\Drivers\Source\MyGPIO.c
 .\objects\mygpio.o: ..\FichiersCode\Drivers\Source\../Include/MyGPIO.h
 .\objects\mygpio.o: C:\Users\lopez\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\Include\stm32f10x.h
-.\objects\mygpio.o: .\RTE\_Reel\RTE_Components.h
+.\objects\mygpio.o: .\RTE\_Simulation\RTE_Components.h
 .\objects\mygpio.o: C:\Users\lopez\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\core_cm3.h
 .\objects\mygpio.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\objects\mygpio.o: C:\Users\lopez\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_version.h
