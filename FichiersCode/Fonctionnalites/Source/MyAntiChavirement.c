47f60fe7eef36730a985dfdded3a2d6c98604bc6
maiamn <maia.mangin25@gmail.com>
2021-11-15 16:05:37 +0100

    Configuration du SPI et de l'ADXL dans la fonctionnalité anti chavirement

--- FichiersCode/Fonctionnalites/Source/MyAntiChavirement.c
+++ FichiersCode/Fonctionnalites/Source/MyAntiChavirement.c
@@ -4,3 +4,13 @@
 #include "MyGPIO.h"
 #include "MySPI.h"
 #include "serviceADXL.h"
+
+// Declaration d'une structure SPI 
+SPI_TypeDef * SPI1 ; 
+
+// Configuration du SPI et de l'ADXLa
+MySPI_Config(SPI1) ; 
+MyADXL_Config(SPI1) ;
+
+// 
+
