08bf613e0138c351707d1c9d83b7468ed0b6b548
Luis97Lopez <35906037+Luis97Lopez@users.noreply.github.com>
2021-11-18 17:44:19 +0100

    Revert "IT"
    This reverts commit 8c7cbe69ef537edf002d369e59bd67ffc665091b.

--- FichiersCode/Fonctionnalites/Source/MyBordageAutomatique.c
+++ FichiersCode/Fonctionnalites/Source/MyBordageAutomatique.c
@@ -1,40 +1,47 @@
 #include "MyBordageAutomatique.h"
 #include "stm32f10x.h"
 
-MyTimer_Struct_TypeDef timer;
-MyGPIO_Struct_TypeDef gpio;
+MyTimer_Struct_TypeDef timer1, timer2;
+MyGPIO_Struct_TypeDef gpio, led;
 
 int count = 0;
 void Callback(){
-	count++;
+
 }
 
 
 void MyBordageAutomatique_setup(void){
+	// Configuration of TIMER1
+	timer1.TimId = TIM1;
+	timer1.ARR = 7200-1;
+	timer1.PSC = 1000-1;
+	MyTimer_Base_Init(&timer1);
+	MyTimer_ActiveIT(timer1.TimId, 0, Callback);
+	
 	// Configuration of TIMER2
-	timer.TimId = TIM1;
-	timer.ARR = 1435;
-	timer.PSC = 1;
-	MyTimer_Base_Init(&timer);
-	MyTimer_Enable_EncoderMode(timer.TimId);
-	MyTimer_ActiveIT(TIM1, 0, Callback);
-		
+	timer2.TimId = TIM2;
+	timer2.ARR = 1440;
+	timer2.PSC = 0;
+	MyTimer_Base_Init(&timer2);
+
+	MyTimer_Enable_EncoderMode(timer2.TimId);
+	
 	// Using Channel1 and Channel2 of TIMER2 (Channel1 = PA0, Channel1 = PA1)
 	gpio.GPIO = GPIOA;
 	gpio.GPIO_Conf = In_PullDown;
 	
-	gpio.GPIO_Pin = 8;
+	gpio.GPIO_Pin = 0;
 	MyGPIO_Init(&gpio);
 	
-	gpio.GPIO_Pin = 9;
+	gpio.GPIO_Pin = 1;
 	MyGPIO_Init(&gpio);
 	
 	//gpio.GPIO_Pin = 2;
 	//MyGPIO_Init(&gpio);
 	
 	// Configuration Encoder Mode in this two channels
-	MyTimer_Set_EncoderMode(timer.TimId, 1);
-	MyTimer_Set_EncoderMode(timer.TimId, 2);
+	MyTimer_Set_EncoderMode(timer2.TimId, 1);
+	MyTimer_Set_EncoderMode(timer2.TimId, 2);
 }
 
 void MyBordageAutomatique_start(void){
