82d429f6244a9f4db514ce0b7912c3b9104278f3
Luis97Lopez <lpzr97@gmail.com>
2021-11-18 15:15:58 +0100

    TESTS

--- FichiersCode/Fonctionnalites/Source/MyBordageAutomatique.c
+++ FichiersCode/Fonctionnalites/Source/MyBordageAutomatique.c
@@ -12,21 +12,25 @@ void Callback(){
 
 void MyBordageAutomatique_setup(void){
 	// Configuration of TIMER2
-	timer.TimId = TIM2;
+	timer.TimId = TIM1;
 	timer.ARR = 1435;
 	timer.PSC = 1;
 	MyTimer_Base_Init(&timer);
 	MyTimer_Enable_EncoderMode(timer.TimId);
+	MyTimer_ActiveIT(TIM1, 0, Callback);
 		
 	// Using Channel1 and Channel2 of TIMER2 (Channel1 = PA0, Channel1 = PA1)
 	gpio.GPIO = GPIOA;
 	gpio.GPIO_Conf = In_PullDown;
 	
-	gpio.GPIO_Pin = 0;
+	gpio.GPIO_Pin = 8;
 	MyGPIO_Init(&gpio);
 	
-	gpio.GPIO_Pin = 1;
+	gpio.GPIO_Pin = 9;
 	MyGPIO_Init(&gpio);
+	
+	//gpio.GPIO_Pin = 2;
+	//MyGPIO_Init(&gpio);
 	
 	// Configuration Encoder Mode in this two channels
 	MyTimer_Set_EncoderMode(timer.TimId, 1);
@@ -34,5 +38,5 @@ void MyBordageAutomatique_setup(void){
 }
 
 void MyBordageAutomatique_start(void){
-	MyTimer_Base_Start(TIM2);
+	MyTimer_Base_Start(TIM1);
 }
